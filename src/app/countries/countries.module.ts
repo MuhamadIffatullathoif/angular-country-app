0c1a1b583f069b8ce4b8e19bd73434cfac2c511d
MuhamadIffatullathoif <iffatlathoif1997@gmail.com>
2024-01-08 13:44:57 +0700

    07. Page structure and SearchBox Component

--- src/app/countries/countries.module.ts
+++ src/app/countries/countries.module.ts
@@ -1,11 +1,11 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { ByCapitalPageComponent } from './pages/by-capital-page/by-capital-page.component';
-import { ByCountryPageComponent } from './pages/by-country-page/by-country-page.component';
-import { ByRegionPageComponent } from './pages/by-region-page/by-region-page.component';
-import { CountryPageComponent } from './pages/country-page/country-page.component';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {ByCapitalPageComponent} from './pages/by-capital-page/by-capital-page.component';
+import {ByCountryPageComponent} from './pages/by-country-page/by-country-page.component';
+import {ByRegionPageComponent} from './pages/by-region-page/by-region-page.component';
+import {CountryPageComponent} from './pages/country-page/country-page.component';
 import {CountriesRoutingModule} from "./countries-routing.module";
-
+import {SharedModule} from "../shared/shared.module";
 
 
 @NgModule({
@@ -17,7 +17,9 @@ import {CountriesRoutingModule} from "./countries-routing.module";
   ],
   imports: [
     CommonModule,
-    CountriesRoutingModule
+    CountriesRoutingModule,
+    SharedModule
   ]
 })
-export class CountriesModule { }
+export class CountriesModule {
+}
