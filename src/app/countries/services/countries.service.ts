faa9bb9bf126f45e6dacb983cf0387801acd5ba6
MuhamadIffatullathoif <iffatlathoif1997@gmail.com>
2024-01-09 08:18:15 +0700

    24. Keep Data between screen

--- src/app/countries/services/countries.service.ts
+++ src/app/countries/services/countries.service.ts
@@ -1,7 +1,8 @@
 import {Injectable} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
-import {catchError, delay, map, Observable, of} from "rxjs";
+import {catchError, delay, map, Observable, of, tap} from "rxjs";
 import {Country} from "../interfaces/country";
+import {CacheStore} from "../interfaces/cache-store.interface";
 
 @Injectable({
   providedIn: 'root'
@@ -9,6 +10,12 @@ import {Country} from "../interfaces/country";
 export class CountriesService {
 
   private apiUrl: string = 'https://restcountries.com/v3.1';
+
+  public cacheStore: CacheStore = {
+    byCapital: {term: '', countries: []},
+    byCountries: {term: '', countries: []},
+    byRegion: {region: '', countries: []}
+  }
 
   constructor(private httpClient: HttpClient) {
   }
@@ -22,7 +29,10 @@ export class CountriesService {
 
   searchCapital(term: string): Observable<Country[]> {
     const url = `${this.apiUrl}/capital/${term}`;
-    return this.getCountriesRequest(url);
+    return this.getCountriesRequest(url)
+      .pipe(
+        tap(countries => this.cacheStore.byCapital = {term, countries})
+      );
   }
 
   searchCountry(term: string): Observable<Country[]> {
